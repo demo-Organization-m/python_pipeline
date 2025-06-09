f9f8e04bc35419e3581a8fad3c5a3db90a3bd884
Anishadevops <anishacsc2001@gmail.com>
2025-06-09 12:15:02 +0530

    update files

--- Fast_API/main.py
+++ Fast_API/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from Fast_API.api.router import blog
-from router import blog_post
+from api.router import blog
 
 app = FastAPI()
 app.include_router(blog.router)
-app.include_router(blog_post.router)
+# app.include_router(blog_post.router)
 
 @app.get('/hello')
 def index():
